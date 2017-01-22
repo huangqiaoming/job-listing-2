1fbe0b9b53082c529f308bcc0937da108e16af88
huangqiaoming <carolinehuang.aiesec@gmail.com>
2017-01-23 05:30:55 +0800

    step6 order by wage

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -10,8 +10,16 @@ class Admin::JobsController < ApplicationController
     end
   end
   def index
-    @jobs = Job.all
-  end
+      @jobs = case params[:order]
+              when 'by_lower_bound'
+                Job.published.order('wage_lower_bound DESC')
+              when 'by_upper_bound'
+                Job.published.order('wage_upper_bound DESC')
+              else
+                Job.published.recent
+              end
+    end
+
   def new
     @job = Job.new
   end
