d699b8538ac7a9cae332a74cfbe2a78ac87a7b2b
huangqiaoming <carolinehuang.aiesec@gmail.com>
2017-01-22 15:41:38 +0800

    implement step5

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,8 +1,13 @@
 class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   before_action :require_is_admin
+  layout "admin"
   def show
     @job = Job.find(params[:id])
+    if @job.is_hidden
+      flash[:warning] = "This job already archieved"
+      redirect_to root_path
+    end
   end
   def index
     @jobs = Job.all
@@ -33,6 +38,17 @@ def destroy
   @job = Job.find(params[:id])
   @job.destroy
   redirect_to admin_jobs_path
+end
+def publish
+  @job = Job.find(params[:id])
+  @job.publish!
+  redirect_to :back
+end
+
+def hide
+  @job = Job.find(params[:id])
+  @job.hide!
+  redirect_to :back
 end
 
 private
